053a9c9becd9df2d0dad76e19e7df20fdbea4449
hugo <hchemillier@juniorisep.com>
2020-11-25 19:41:38 +0100

    add lombok library

--- src/main/java/com/mycompany/myapp/service/dto/MissionUserDTO.java
+++ src/main/java/com/mycompany/myapp/service/dto/MissionUserDTO.java
@@ -2,43 +2,16 @@ package com.mycompany.myapp.service.dto;
 
 import com.mycompany.myapp.domain.Company;
 import com.mycompany.myapp.service.view.MissionUserView;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MissionUserDTO implements MissionUserView {
     private Long id;
     private String name;
     private Company company;
 
-    public MissionUserDTO() {
-    }
-
-    public MissionUserDTO(Long id, String name, Company company) {
-        this.id = id;
-        this.name = name;
-        this.company = company;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public Company getCompany() {
-        return company;
-    }
-
-    public void setCompany(Company company) {
-        this.company = company;
-    }
 }
