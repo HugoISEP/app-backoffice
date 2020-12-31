bfd56fd004a3063e743a3bd04c53e12d3f7d6ea9
Alexis Tonneau <atonneau@juniorisep.com>
2020-12-31 15:22:12 +0100

    As configuration

--- src/main/java/com/mycompany/myapp/config/FirebaseConfiguration.java
+++ src/main/java/com/mycompany/myapp/config/FirebaseConfiguration.java
@@ -6,15 +6,14 @@ import com.google.firebase.FirebaseOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-@Service
-public class FirebaseConfiguration {
+@Configuration public class FirebaseConfiguration {
     @Value("${firebase}")
     private String firebaseConfigPath;
 
