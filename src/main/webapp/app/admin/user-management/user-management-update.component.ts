187518f7821dd0109f06dd9fbd4e2984939c9c50
hugo <hchemillier@juniorisep.com>
2021-02-19 12:49:33 +0100

    null as default value for select

--- src/main/webapp/app/admin/user-management/user-management-update.component.ts
+++ src/main/webapp/app/admin/user-management/user-management-update.component.ts
@@ -51,7 +51,6 @@ export class UserManagementUpdateComponent implements OnInit {
             this.companies = companies.body!;
           });
           this.user.activated = true;
-          this.user.authorities = ['ROLE_MANAGER'];
         }
         this.updateForm(user);
       }
