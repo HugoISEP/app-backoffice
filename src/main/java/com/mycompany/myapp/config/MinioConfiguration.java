0f05f3b9c1d552041dc495b4504792a2ea5b734c
Alexis Tonneau <atonneau@juniorisep.com>
2021-05-17 17:00:48 +0200

    Add in yaml

--- src/main/java/com/mycompany/myapp/config/MinioConfiguration.java
+++ src/main/java/com/mycompany/myapp/config/MinioConfiguration.java
@@ -7,12 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.Resource;
 
-import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 
 import static com.mycompany.myapp.config.Constants.LOGO_BUCKET;
 
@@ -26,47 +22,37 @@ public class MinioConfiguration {
     String minioUrl;
     @Value("classpath:minio/policy-config.json")
     Resource config;
+    @Value("#{new Boolean('${spring.minio.mandatory}')}")
+    boolean mandatory;
+
 
     Logger logger = LoggerFactory.getLogger(MinioConfiguration.class);
 
     @Bean
-    @Profile("!prod")
     public MinioClient generateMinioClientNotProd() throws MinioException {
         try {
-            return getMinioClient();
+            MinioClient minio = MinioClient.builder()
+                .endpoint(minioUrl)
+                .credentials(accessKey, accessSecret)
+                .build();
+
+            if(!minio.bucketExists(BucketExistsArgs.builder().bucket(LOGO_BUCKET).build())){
+                minio.makeBucket(MakeBucketArgs.builder().bucket(LOGO_BUCKET).build());
+            }
+            minio.setBucketPolicy(SetBucketPolicyArgs.builder()
+                .config(ResourceReader.asString(config))
+                .bucket(LOGO_BUCKET)
+                .build());
+            return minio;
+
         } catch (Exception e) {
+            if (mandatory) {
+                throw new MinioException(e.getMessage());
+            }
             logger.error("Failed to start minio");
             logger.debug(e.getMessage());
             return null;
         }
-    }
-
-    @Bean
-    @Profile("prod")
-    public MinioClient generateMinioClientProd() throws MinioException {
-        try {
-            return getMinioClient();
-        } catch (Exception e) {
-            throw new MinioException(e.getMessage());
-        }
-    }
-
-
-
-    public MinioClient getMinioClient() throws IOException, NoSuchAlgorithmException, InvalidKeyException, MinioException {
-        MinioClient minio = MinioClient.builder()
-            .endpoint(minioUrl)
-            .credentials(accessKey, accessSecret)
-            .build();
-
-        if(!minio.bucketExists(BucketExistsArgs.builder().bucket(LOGO_BUCKET).build())){
-            minio.makeBucket(MakeBucketArgs.builder().bucket(LOGO_BUCKET).build());
-        }
-        minio.setBucketPolicy(SetBucketPolicyArgs.builder()
-            .config(ResourceReader.asString(config))
-            .bucket(LOGO_BUCKET)
-            .build());
-        return minio;
     }
 
 }
