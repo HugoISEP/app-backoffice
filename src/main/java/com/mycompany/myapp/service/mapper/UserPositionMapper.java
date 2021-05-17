252bcbd1a76981ce2c2933b2ab180534f11c6bd7
Alexis Tonneau <atonneau@juniorisep.com>
2021-05-17 13:03:39 +0200

    Fix mapping

--- src/main/java/com/mycompany/myapp/service/mapper/UserPositionMapper.java
+++ src/main/java/com/mycompany/myapp/service/mapper/UserPositionMapper.java
@@ -3,10 +3,15 @@ package com.mycompany.myapp.service.mapper;
 import com.mycompany.myapp.domain.UserPosition;
 import com.mycompany.myapp.service.dto.UserPositionDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface UserPositionMapper {
+
+    @Mapping(target = "user", ignore = true)
     UserPositionDto asDto(UserPosition in);
+
+    @Mapping(target = "user", ignore = true)
     UserPosition fromDto(UserPositionDto in);
 }
