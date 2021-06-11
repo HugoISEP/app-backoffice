be61d4349834dd9734c9929a6c5d4cb950a61282
Théo <theo.martinez@isep.fr>
2021-06-11 17:37:16 +0200

    Fix material theme import

--- src/main/webapp/app/vendor.ts
+++ src/main/webapp/app/vendor.ts
@@ -1,2 +1,4 @@
 /* after changing this file run 'yarn run webpack:build' */
 import '../content/scss/vendor.scss';
+import "@angular/material";
+import "@angular/material/prebuilt-themes/indigo-pink.css";
